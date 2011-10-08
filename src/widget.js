dd94473b498184a99527e5d6afd6e1c7007c4709
Niklas Schrader <niklas.schrader@gmail.com>
2011-10-08 17:44:04 +0200

    update

--- src/widget.js
+++ src/widget.js
@@ -2,3 +2,19 @@
 // Vårt tokfestliga objekt
 
 widget = new Object();
+
+var datMap;
+self.onload = function() {
+datMap = new google.maps.Map(document.getElementById("googlemap"),
+  {zoom: 4,
+   center: new google.maps.LatLng(65, 10),
+   mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+google.maps.event.addListener(datMap, 'bounds_changed', mapMoved);
+}
+
+
+function mapMoved(event) {
+document.forms[0].position.value = event + "";
+}
