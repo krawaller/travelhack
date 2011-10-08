ba109e88dd44db3c61102b0dfd18803043dda4ef
Niklas Schrader <niklas.schrader@gmail.com>
2011-10-08 17:59:05 +0200

    update

--- src/widget.js
+++ src/widget.js
@@ -3,18 +3,47 @@
 
 widget = new Object();
 
-var datMap;
-self.onload = function() {
-datMap = new google.maps.Map(document.getElementById("googlemap"),
-  {zoom: 4,
-   center: new google.maps.LatLng(65, 10),
-   mapTypeId: google.maps.MapTypeId.ROADMAP
+var map;
+
+function initialize() {
+  var myOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(geoip_latitude(), geoip_longitude()),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  var map = new google.maps.Map(document.getElementById('googlemap'), myOptions);
+  var geocoder = new google.maps.Geocoder();
+
+  var marker;
+  google.maps.event.addListener(map, 'click', function(e) {
+    if(!marker){
+      marker = new google.maps.Marker({
+        position: e.latLng,
+        map: map
+      });
+    } else {
+      marker.setPosition(e.latLng);
+    }
+
+    // getElementById('placeLatLng').innerHTML = [e.latLng.lat(), e.latLng.lng()].join(", ");
+    geocoder.geocode({
+        latLng: e.latLng
+      },
+      function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            document.forms[0].position.value = results[0].formatted_address;
+          }
+        } else {
+          // alert("Geocoder failed due to: " + status);
+          // document.forms[0].position = "Fel! Testa igen!";
+        }
+      }
+    );
+    
+    map.panTo(e.latLng);
   });
-
-google.maps.event.addListener(datMap, 'bounds_changed', mapMoved);
 }
 
-
-function mapMoved(event) {
-document.forms[0].position.value = event + "";
-}
+google.maps.event.addDomListener(window, 'load', initialize);
