50d0179640e5dae5ce9c0fb03a1865e7dce54f54
KungD <david@krawaller.se>
2011-10-08 20:09:24 +0200

    documentation of resihop functions

--- src/resihop.js
+++ src/resihop.js
@@ -2,6 +2,12 @@
 	ensureArray = function(arg){ return Array.isArray(arg) ? arg : arg ? [arg] : [];}
 	widget = window.widget || {}
 	widget.resihop = {
+		/**
+		 * from and to are adresses in strings
+		 * when is date in ISO format (YYYY-MM-DD)
+		 * callback is function that will be called with the result
+		 * the result is an array of objects for all matching trips (so might be empty)
+		 */
 		searchTrip: function(from,to,when,callback){
 			url = "http://resihop.nu/search?from=FROM&to=TO&when=WHEN"
 			.replace("FROM",encodeURIComponent(from))
@@ -17,6 +23,17 @@
 				callback(result);
 			});
 		},
+		/**
+		 * from and to are adresses in strings
+		 * when is date in ISO format (YYYY-MM-DD)
+		 * callback is function that will be called with the result
+		 * got_car is 1 or 0 depending on whether you are looking for ride or passengers
+		 * email is in text string, needed to retrieve edit code for trip at resihop
+		 * phone is phone number in string, shown to other travellers at resihop
+		 * callback is function that will be called with the result
+		 * this result is an object, if post was successful it has ok set to true and code set to the edit code from resihop
+		 * if failed, the object has an error prop with an error object
+		 */
 		postTrip: function(from,to,when,got_car,name,email,phone,details,callback){
 			url = "http://dev.resihop.nu/addtrip?from=FROM&to=TO&when=WHEN&got_car=GOT_CAR&name=NAME&email=EMAIL&phone=PHONE&details=DETAILS"
 			.replace("FROM",encodeURIComponent(from))
